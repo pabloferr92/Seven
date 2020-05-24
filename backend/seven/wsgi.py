91224b856c9775ef5ecbd0641c650b6383727033
pabloferr92 <pabloferr92@gmail.com>
2020-05-23 21:22:27 -0300

    correcao wsgi

--- backend/seven/wsgi.py
+++ backend/seven/wsgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.append("F://PROJETOS//Seven//backend")
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seven.settings')
 
 application = get_wsgi_application()
+
+print(application)
